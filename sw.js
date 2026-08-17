ae19962b13b088ac6f0c5d417bb8e1772edf0bfe
branzfamily01 <branz.family.01@gmail.com>
2026-08-17 13:53:18 +0900

    Cache corrected growth atlases v10

--- sw.js
+++ sw.js
@@ -1,23 +1,26 @@
-const CACHE='ocean-study-quest-v9';
+const CACHE='ocean-study-quest-v10';
 const ASSETS=[
   "./",
   "./index.html",
-  "./css/parts/part1.css?v=8",
-  "./css/parts/part2.css?v=8",
-  "./css/parts/part3.css?v=8",
-  "./css/parts/part4.css?v=8",
-  "./js/creatures/anglerfish.js?v=8",
-  "./js/creatures/bundle1.js?v=8",
-  "./js/creatures/bundle2.js?v=8",
-  "./js/creatures/bundle3.js?v=8",
-  "./js/data.js?v=8",
-  "./js/storage.js?v=8",
-  "./js/audio.js?v=8",
-  "./js/app-gzip.js?v=8",
-  "./js/app-loader.js?v=8",
-  "./js/xp-recovery.js?v=8",
-  "./manifest.webmanifest?v=8",
-  "./assets/ui/icon.svg?v=8"
+  "./css/parts/part1.css?v=10",
+  "./css/parts/part2.css?v=10",
+  "./css/parts/part3.css?v=10",
+  "./css/parts/part4.css?v=10",
+  "./css/parts/atlas.css?v=10",
+  "./js/creatures/anglerfish.js?v=10",
+  "./js/creatures/bundle1.js?v=10",
+  "./js/creatures/bundle2.js?v=10",
+  "./js/creatures/bundle3.js?v=10",
+  "./js/data.js?v=10",
+  "./js/storage.js?v=10",
+  "./js/audio.js?v=10",
+  "./js/app-gzip.js?v=10",
+  "./js/app-loader.js?v=10",
+  "./js/xp-recovery.js?v=10",
+  "./assets/growth/growth-atlas-a.webp?v=10",
+  "./assets/growth/growth-atlas-b.webp?v=10",
+  "./manifest.webmanifest?v=10",
+  "./assets/ui/icon.svg?v=10"
 ];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
